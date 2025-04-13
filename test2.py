4bad9d85f38f6e74fcab15f133e54fc764cacc59
abolfazlpoormohammadi <Abolfazlpouri1384@gmail.com>
2025-04-13 17:24:29 +0330

    تمرین عملگر ها

---
+++ test2.py
@@ -0,0 +1,30 @@
+
+print(3+2)
+print(5*4)
+print(20/5)
+print(20//5)
+print(10%5)
+print(2-1)
+#----------------------------
+#----------------------------
+number1=2
+number2=3
+num3=2
+print(number1==number2)
+print(number1>=number2)
+print(number1<=number2)
+print(number1!=number2)
+print(number1>number2)
+print(number1<number2)
+#------------------------------
+#------------------------------
+if (num3 == number1) & (num3 > number2):
+    print("dorost ast")
+else:print("eshtebah ast")
+if (num3 == number1) | (number2 > num3):
+    print("dorost ast")
+else:print("eshtebah ast")
+#-----------------------------
+adad=True
+print(not adad)
+
